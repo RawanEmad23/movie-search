404b0b4d2d1f581361cbeccb9ad0d48a0bded870
RawanEmad23 <re0999578@gmail.com>
2025-12-03 12:12:20 +0200

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import {HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ResultsPage from './pages/ResultsPage';
 import Details from './pages/Details';
 import FavoritesPage from './pages/Favorites';
-
-
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-<Route path="/search/" element={<ResultsPage />} />
-  <Route path="/movie/:id/" element={<Details />} />
-  <Route path="/favorites/" element={<FavoritesPage />} />
+        <Route path="/search" element={<ResultsPage />} />
+        <Route path="/movie/:id" element={<Details />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
       </Routes>
     </Router>
   );
